111545ea31cb0cd6183ecddf5fab062b3bf61bc6
nandini965 <nandhini965@gmail.com>
2023-09-16 19:54:26 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -43,9 +43,10 @@ resource "aws_security_group" "main" {
       encrypted  = true
       kms_key_id = var.kms_arn
     }
-       user_data  = base64encode(templatefile("${path.module}/userdata.sh", {
-        rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
-      }))
+    user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+      rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
+    }))
+
     }
 
 
