e352651474bb253713d76f841c499e06119abb2c
nandini965 <nandhini965@gmail.com>
2023-09-16 19:32:25 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "main" {
     root_block_device {
       encrypted  = true
       kms_key_id = var.kms_arn
-      user_data  = base64encode(templatefile("${path.module}/userdata.sh", {
+       user_data  = base64encode(templatefile("${path.module}/userdata.sh", {
         rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
       }))
     }
