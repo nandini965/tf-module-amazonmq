d537e148aa02fe483dc0335b68ec963972f3bcd8
nandini965 <nandhini965@gmail.com>
2023-09-16 19:43:59 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -38,10 +38,11 @@ resource "aws_security_group" "main" {
     vpc_security_group_ids = [aws_security_group.main.id]
 
     tags = merge(var.tags, { Name = "${var.name}-${var.env}" })
-  }
+
     root_block_device {
       encrypted  = true
       kms_key_id = var.kms_arn
+    }
        user_data  = base64encode(templatefile("${path.module}/userdata.sh", {
         rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
       }))
