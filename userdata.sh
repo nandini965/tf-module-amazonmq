b4d8677a627a2d7c65c028e402f8f732d0da31de
nandini965 <nandhini965@gmail.com>
2023-09-16 19:18:31 +0530

    filebeat

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd/opt
-git clone "https://github.com/nandini965/tf-module-amazonmq.git"
+git clone "https://github.com/nandini965/tf-module-amazon-mq.git"
 cd roboshop-shell
 bash rabbitmq.sh ${rabbitmq_app_user_password} &>>/opt/roboshop.log
