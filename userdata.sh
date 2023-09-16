79d13f23c7b27fbab2ec57b2572c0de85dd95905
nandini965 <nandhini965@gmail.com>
2023-09-16 22:46:19 +0530

    filebeat

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,6 @@
-
 #!/bin/bash
 
 cd /opt
-git clone "https://github.com/nandini965/roboshop-shell"
+git clone https://github.com/nandini965/roboshop-shell.git
 cd roboshop-shell
 bash rabbitmq.sh ${rabbitmq_appuser_password} &>>/opt/roboshop.log
